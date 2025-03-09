8ba75508678fd13cc82b2cfc2f767896c5f34418
Squiro <alesio_sinopoli@hotmail.com>
2025-03-09 17:23:15 -0300

    Delete docusarus images

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -57,7 +57,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/LED_Flashlights.jpg",
     navbar: {
       title: "Linternita",
       logo: {
